e47d239a5b3646bf6dce620ab67c6259c186864b
Sam Phillips <samdphillips@gmail.com>
2023-08-02 12:10:58 -0700

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define name "epr")
+(define collection "epr")
+(define version "0.0.1")
+(define deps '("base" "rhombus-prototype"))
+(define build-deps '("base"))
+(define pkg-authors '(samdphillips@gmail.com))
+(define license 'Apache-2.0)
