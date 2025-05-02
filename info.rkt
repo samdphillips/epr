fb4ea6185e4dacc5e6812ccd83faa6e6fe4346d7
Sam Phillips <samdphillips@gmail.com>
2025-05-02 15:11:46 -0700

    Add syncvar-lib dep

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define name "epr")
 (define collection "epr")
 (define version "0.0.1")
-(define deps '("base" "rhombus-lib"))
+(define deps '("base" "rhombus-lib" "syncvar-lib"))
 (define build-deps '("base"))
 (define pkg-authors '(samdphillips@gmail.com))
 (define license 'Apache-2.0)
